11317268125887adaa52faca62c144fe0c2b9b0e
Luna <lunaxu99@outlook.com>
2024-05-21 12:56:22 -0600

    update a1

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,7 +7,8 @@ import Lab3 from "./Lab3";
 export default function Labs() {
     return (
         <div>
-            <h1>Labs</h1>
+            <h1>Xinyi Xu</h1>
+            <a id="wd-github" href="https://github.com/last-dino/kanbas-react-web-app">Github Repo</a>
             <TOC />
             <Routes>
                 <Route path="/" element={<Navigate to="Lab1" />} />
