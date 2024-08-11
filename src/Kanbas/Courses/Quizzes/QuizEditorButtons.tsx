fc2e32655a63aaa3a4260048deab85cccf6e989a
Luna <lunaxu99@outlook.com>
2024-08-10 20:39:01 -0600

    fixed tab switch

--- src/Kanbas/Courses/Quizzes/QuizEditorButtons.tsx
+++ src/Kanbas/Courses/Quizzes/QuizEditorButtons.tsx
@@ -19,10 +19,10 @@ export default function QuizEditorButtons() {
             <div id="wd-css-navigating-with-tabs">
                 <ul className="nav nav-tabs">
                     <li className="nav-item">
-                        <a className="nav-link active" onClick={navigateToDetailEditor}>Details</a>
+                        <a className={`nav-link ${pathname.includes("QuizEditor") ? "active" : "text-danger"}`} onClick={navigateToDetailEditor}>Details</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link text-danger" onClick={navigateToQuestionEditor}>Questions</a>
+                        <a className={`nav-link ${pathname.includes("QuestionEditor") ? "active" : "text-danger"}`} onClick={navigateToQuestionEditor}>Questions</a>
                     </li>
                 </ul>
             </div>
